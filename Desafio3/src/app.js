16240081a4c7a1d1640d740fd498c933d188d331
Martin Rivero <canuzamdq@gmail.com>
2023-04-30 10:54:37 -0300

    subida tercer desadio

---
+++ Desafio3/src/app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import ProductManager from '../productManager.js'
+
+const app = express();
+const productManager = new ProductManager();
+
+app.use(express.urlencoded({extended:true}));   
+
+
+// req.query: permite establecer un limiete de productos a mostrar. Ej:   http://localhost:8080/products/?limit=4
+
+app.get('/products', async (req, res) =>{
+    try {
+        let allProducts = await productManager.getProducts();
+        let limit = req.query.limit;
+
+        !limit ? res.send(allProducts) : res.send(allProducts.splice(0, limit));
+
+    } catch(err) {
+        console.log(err);
+    }
+    
+})
+
+//req params: permite buscar un poructo por su ID- EJ: http://localhost:8080/products/2
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        let productById = await productManager.getProductById(parseInt(req.params.id));
+        res.send(productById);
+    }catch(err) {
+        console.log(err);
+    }
+})
+
+
+
+
+
+app.listen(8080, () => {
+    console.log('Escuchando puerto 8080...');
+})
